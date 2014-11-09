05269d90550c76abba13892a4d2a4e71d6d02ce9
Cloud User <ec2-user@ip-172-31-49-190.ec2.internal>
2014-11-09 16:43:16 -0500

    added notifications client

---
+++ pi/notifications-client/notifications-client.py
@@ -0,0 +1,16 @@
+#!/bin/python 
+
+
+import urllib2
+import json 
+
+class NotificationsClient:
+
+	url="http://localhost:8080/notifications/"
+
+	def getForSIDD(self, sidd):
+		output = urllib2.urlopen(self.url  + sidd).read()
+		notificationsForUser = json.loads(output)
+		return notificationsForUser		
+	
+print NotificationsClient().getForSIDD("scmallo")
