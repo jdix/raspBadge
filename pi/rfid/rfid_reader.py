6d3c76924dbcb1f54178d1bdea4d863b56342a30
Susan Pitcher <susanpitcher@susans-air.home>
2014-11-28 20:23:16 +0000

    halibut

--- pi/rfid/rfid_reader.py
+++ pi/rfid/rfid_reader.py
@@ -1,9 +1,41 @@
 #!/bin/python
-
+import rfid
 
 class RFID:
     def __init__(self):
-        pass
+       	self.cards = {
+ 		"2B53B49B"       : "whaleygeek", 
+ 		"04982B29EE0280" : "elektor RFID card", 
+  		"EAC85517"       : "white card 1",
+  		"24B1E145"       : "white card 2",
+  		"C2091F58"       : "label 1",
+  		"22F51E58"       : "label 2"
+	} 
+	
 
     def wait_for_next(self):
-        return raw_input('Enter a fake RFID number. (12345 is scmallo): ')
+        # Import this module to gain access to the RFID driver
+
+	# fill in this map with the names of your card ID's
+
+  	# wait for a card to be detected as present
+  	print("Waiting for a card...")
+  	rfid.waitTag()
+  	print("Card present")
+
+  	# This demo only uses Mifare cards
+  	if not rfid.readMifare():
+    		print("This is not a mifare card")
+  	else:
+    	# What type of Mifare card is it? (there are different types)
+    	print("Card type:" + rfid.getTypeName())
+
+    	# look up the unique ID to see if we recognise the user
+    	uid = rfid.getUniqueId()
+
+  	# wait for the card to be removed
+  	print("Waiting for card to be removed...")
+  	rfid.waitNoTag()
+  	print("Card removed")
+
+	return uid
